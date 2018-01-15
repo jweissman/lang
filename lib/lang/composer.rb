a04c78c71d9ee4ea354dfa75707eaf9c4f6507cc
Joseph Weissman <joe@bignerdranch.com>
2018-01-15 11:55:36 -0500

    try to decompose resolution further

--- lib/lang/composer.rb
+++ lib/lang/composer.rb
@@ -15,21 +15,38 @@ module Lang
       end
     end
 
+    protected
+
     def resolve_children(meth, *children)
       children.map do |child|
         if child.is_a?(Array)
-          resolved_child = resolve(*child)
-          if self.class.post_resolution_hooks.any?
-            self.class.post_resolution_hooks.each do |method:, except:|
-              unless meth == except
-                resolved_child = send(method, resolved_child)
-              end
-            end
-          end
-          resolved_child
+          resolve_child(meth, child)
         else
           child
         end
+      end
+    end
+
+    def resolve_child(meth, child)
+      resolved_child = resolve(*child)
+      resolved_child = run_post_resolve_hooks(meth, resolved_child)
+      resolved_child
+    end
+
+    def run_post_resolve_hooks(meth, child)
+      post_resolve_hooks_for(meth).each do |method:, **opts|
+        child = send(method, child)
+      end
+      child
+    end
+
+    def post_resolve_hooks_for(meth)
+      if self.class.post_resolution_hooks.any?
+        self.class.post_resolution_hooks.select do |method:, except:|
+          meth != except
+        end
+      else
+        []
       end
     end
 
